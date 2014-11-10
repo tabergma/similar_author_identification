8b9511c4c3c4d5ddab7a96eabc863ba16521c5d7
Tanja Bergmann <tabergma@gmail.com>
2014-11-10 11:59:30 +0100

    Merge main.

--- python/Main.py
+++ python/Main.py
@@ -1,12 +1,19 @@
-# start program
-
 import Read
-import Tokenizer
+from Tokenizer import get_word_frequency
+from sklearn import svm
 
 
 def main():
-	# do something
-	pass
+	X = [[0, 0], [1, 1]]
+	y = [0, 1]
+
+	clf = svm.SVC()
+	clf.fit(X, y)
+
+	content = ["This is a n mn i test"]
+
+	print get_word_frequency(content)
+
 
 if __name__ == "__main__":
 	main()
