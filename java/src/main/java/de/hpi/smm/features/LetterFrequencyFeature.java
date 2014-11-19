6eef86c10862e2b044487a0766e7218ccf4b4728
Tanja Bergmann <tabergma@gmail.com>
2014-11-19 12:26:49 +0100

    Letter Frequency is relative now.

--- java/src/main/java/de/hpi/smm/features/LetterFrequencyFeature.java
+++ java/src/main/java/de/hpi/smm/features/LetterFrequencyFeature.java
@@ -8,45 +8,52 @@ import java.util.Map;
 
 public class LetterFrequencyFeature {
 
-    Map<Character, Float> letterFrequency = new HashMap<Character, Float>();
+    private Map<Character, Float> letterFrequencyMap = new HashMap<Character, Float>();
+    private Float letterCount = 0.f;
 
     public LetterFrequencyFeature() {
-        letterFrequency.put('A', 0.f);
-        letterFrequency.put('B', 0.f);
-        letterFrequency.put('C', 0.f);
-        letterFrequency.put('D', 0.f);
-        letterFrequency.put('E', 0.f);
-        letterFrequency.put('F', 0.f);
-        letterFrequency.put('G', 0.f);
-        letterFrequency.put('H', 0.f);
-        letterFrequency.put('I', 0.f);
-        letterFrequency.put('J', 0.f);
-        letterFrequency.put('K', 0.f);
-        letterFrequency.put('L', 0.f);
-        letterFrequency.put('M', 0.f);
-        letterFrequency.put('N', 0.f);
-        letterFrequency.put('O', 0.f);
-        letterFrequency.put('P', 0.f);
-        letterFrequency.put('Q', 0.f);
-        letterFrequency.put('R', 0.f);
-        letterFrequency.put('S', 0.f);
-        letterFrequency.put('T', 0.f);
-        letterFrequency.put('U', 0.f);
-        letterFrequency.put('V', 0.f);
-        letterFrequency.put('W', 0.f);
-        letterFrequency.put('X', 0.f);
-        letterFrequency.put('Y', 0.f);
-        letterFrequency.put('Z', 0.f);
+        letterFrequencyMap.put('A', 0.f);
+        letterFrequencyMap.put('B', 0.f);
+        letterFrequencyMap.put('C', 0.f);
+        letterFrequencyMap.put('D', 0.f);
+        letterFrequencyMap.put('E', 0.f);
+        letterFrequencyMap.put('F', 0.f);
+        letterFrequencyMap.put('G', 0.f);
+        letterFrequencyMap.put('H', 0.f);
+        letterFrequencyMap.put('I', 0.f);
+        letterFrequencyMap.put('J', 0.f);
+        letterFrequencyMap.put('K', 0.f);
+        letterFrequencyMap.put('L', 0.f);
+        letterFrequencyMap.put('M', 0.f);
+        letterFrequencyMap.put('N', 0.f);
+        letterFrequencyMap.put('O', 0.f);
+        letterFrequencyMap.put('P', 0.f);
+        letterFrequencyMap.put('Q', 0.f);
+        letterFrequencyMap.put('R', 0.f);
+        letterFrequencyMap.put('S', 0.f);
+        letterFrequencyMap.put('T', 0.f);
+        letterFrequencyMap.put('U', 0.f);
+        letterFrequencyMap.put('V', 0.f);
+        letterFrequencyMap.put('W', 0.f);
+        letterFrequencyMap.put('X', 0.f);
+        letterFrequencyMap.put('Y', 0.f);
+        letterFrequencyMap.put('Z', 0.f);
     }
 
     public void evaluateLetterFrequency(String text) {
         text = text.toUpperCase();
         for (char c : text.toCharArray())
-            if (letterFrequency.containsKey(c))
-                letterFrequency.put(c, letterFrequency.get(c) + 1);
+            if (this.letterFrequencyMap.containsKey(c))
+                this.letterFrequencyMap.put(c, this.letterFrequencyMap.get(c) + 1);
     }
 
     public List<Float> getLetterFrequency() {
-        return new ArrayList<Float>(letterFrequency.values());
+        List<Float> letterFrequencyList = new ArrayList<Float>(this.letterFrequencyMap.values());
+
+        for(int i = 0; i < letterFrequencyList.size(); i++) {
+            letterFrequencyList.add(i, letterFrequencyList.get(i) / this.letterCount);
+        }
+
+        return letterFrequencyList;
     }
 }
