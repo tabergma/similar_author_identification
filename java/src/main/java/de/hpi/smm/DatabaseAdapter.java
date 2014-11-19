1d55b8c4086bab18f16cff5afc320c18d21b5488
Joseph B <joseph.bethge@student.hpi.uni-potsdam.de>
2014-11-19 12:03:08 +0100

    added ngdbc.jar, added simple database adapter

---
+++ java/src/main/java/de/hpi/smm/DatabaseAdapter.java
@@ -0,0 +1,86 @@
+package de.hpi.smm;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DatabaseAdapter {
+
+    static String urlFormat = "jdbc:sap://%s:3%s15/";
+
+    private Connection connection = null;
+
+    private String schema = null;
+
+    DatabaseAdapter(){
+    }
+
+    static DatabaseAdapter getSmaAdapter(){
+        DatabaseAdapter databaseAdapter = new DatabaseAdapter();
+
+        String ip = "141.89.225.134";
+        String instance = "03";
+        String user = "SMA1415";
+        String password = "Popcorn54";
+
+        databaseAdapter.open(ip, instance, user, password);
+        databaseAdapter.setSchema("SYSTEM");
+
+        return databaseAdapter;
+    }
+
+    public int executeUpdate(String statement){
+        try {
+            Statement sqlStatement = this.connection.createStatement();
+            return sqlStatement.executeUpdate(statement);
+        } catch (SQLException e) {
+            exceptionCaught(e, statement);
+            return -1;
+        }
+    }
+
+    public ResultSet executeQuery(String statement){
+        try {
+            Statement sqlStatement = this.connection.createStatement();
+            return sqlStatement.executeQuery(statement);
+        } catch (SQLException e) {
+            exceptionCaught(e, statement);
+            return null;
+        }
+    }
+
+    private void exceptionCaught(SQLException e, String statement) {
+        System.out.println(String.format("Exception caught while executing statement: %s", statement));
+        e.printStackTrace();
+    }
+
+    private boolean open(String ip, String instance, String user, String password) {
+        try {
+            Class.forName("com.sap.db.jdbc.Driver");
+
+            String url = String.format(urlFormat, ip, instance);
+
+            this.connection = java.sql.DriverManager.getConnection(url, user, password);
+            this.connection.setAutoCommit(false);
+
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+    }
+
+    public String getSchema() {
+        return schema;
+    }
+
+    public void setSchema(String schema) {
+        this.schema = schema;
+    }
+
+}
--- java/src/main/java/de/hpi/smm/Main.java
+++ java/src/main/java/de/hpi/smm/Main.java
@@ -4,11 +4,26 @@ package de.hpi.smm;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
+
+        String statement = "SELECT POSTCONTENT FROM SYSTEM.WEBPAGE";
+        DatabaseAdapter dbAdapter = DatabaseAdapter.getSmaAdapter();
+        ResultSet rs = dbAdapter.executeQuery(statement);
+        try {
+            if (rs.next()){
+                System.out.println(rs.getString("POSTCONTENT"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
         String filePath = "test.txt";
 
         BufferedReader reader = new BufferedReader(new FileReader(filePath));
