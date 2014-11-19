921199b59cdd80f6421e477f6e312e1c6b34b680
Tanja Bergmann <tabergma@gmail.com>
2014-11-19 17:59:35 +0100

    Add k-means clustering in java. BUT: not working

--- java/src/main/java/de/hpi/smm/Main.java
+++ java/src/main/java/de/hpi/smm/Main.java
@@ -1,28 +1,35 @@
 package de.hpi.smm;
 
 
+import de.hpi.smm.clustering.KMeans;
 import de.hpi.smm.features.FeatureExtractor;
 import de.hpi.smm.helper.DatabaseAdapter;
 import de.hpi.smm.helper.FileWriter;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-
+    public static void main(String[] args) throws Exception {
+        System.out.println("Fetching data...");
         ResultSet rs = getTestSet();
 
         FeatureExtractor featureExtractor = new FeatureExtractor();
         FileWriter fileWriter = new FileWriter("features.txt");
 
+        System.out.println("Extracting features...");
         try {
             while (rs.next()){
                 String content = rs.getString("POSTCONTENT");
                 if (content != null) {
-                    fileWriter.writeFeaturesForDocument(featureExtractor.getFeatures(content));
+                    List<Float> features = featureExtractor.getFeatures(content);
+                    fileWriter.writeFeaturesForDocument(features);
                 }
             }
         } catch (SQLException e) {
@@ -30,11 +37,39 @@ public class Main {
         }
 
         fileWriter.close();
+
+        System.out.println("Performing K-Means...");
+        KMeans kMeans = new KMeans();
+        kMeans.run(readFeatureFile());
     }
 
     private static ResultSet getTestSet() {
         String statement = "SELECT POSTCONTENT FROM SYSTEM.WEBPAGE LIMIT 100000";
         DatabaseAdapter dbAdapter = DatabaseAdapter.getSmaHanaAdapter();
         return dbAdapter.executeQuery(statement);
+    }
+
+    private static List<List<Float>> readFeatureFile() throws IOException {
+        List<List<Float>> list = new ArrayList<List<Float>>();
+
+        BufferedReader br = new BufferedReader(new FileReader("../output/features.txt"));
+        try {
+            String line = br.readLine();
+
+            while (line != null) {
+                List<Float> l = new ArrayList<Float>();
+
+                String[] fs = line.split(" ");
+                for (String f : fs)
+                    l.add(Float.parseFloat(f));
+
+                list.add(l);
+                line = br.readLine();
+            }
+        } finally {
+            br.close();
+        }
+
+        return list;
     }
 }
---
+++ java/src/main/java/de/hpi/smm/clustering/KMeans.java
@@ -0,0 +1,141 @@
+package de.hpi.smm.clustering;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.SequenceFile;
+import org.apache.hadoop.io.Text;
+import org.apache.mahout.clustering.classify.WeightedVectorWritable;
+import org.apache.mahout.clustering.kmeans.KMeansDriver;
+import org.apache.mahout.clustering.kmeans.Kluster;
+import org.apache.mahout.common.HadoopUtil;
+import org.apache.mahout.common.distance.EuclideanDistanceMeasure;
+import org.apache.mahout.math.RandomAccessSparseVector;
+import org.apache.mahout.math.Vector;
+import org.apache.mahout.math.VectorWritable;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class KMeans {
+
+    private final static String OUTPUT_PATH = "output/";
+    private final static String INPUT_PATH = "testdata/";
+    private final static String FEATURE_INPUT_PATH = INPUT_PATH + "points";
+    private final static String CLUSTER_INPUT_PATH = INPUT_PATH + "clusters";
+
+    private final static int k = 6;
+    private final static Configuration conf = new Configuration();
+    private final static Path input = new Path(FEATURE_INPUT_PATH);
+    private final static Path clusterIn = new Path(CLUSTER_INPUT_PATH);
+    private final static Path output = new Path(OUTPUT_PATH);
+    private final static double convergenceDelta = 0.5;
+    private final static int maxIterations = 10;
+    private final static boolean runClustering = true;
+    private final static double clusterClassificationThreshold = 0.5;
+    private final static boolean runSequential = false;
+
+
+    public void run(List<List<Float>> documentFeatures) throws Exception {
+        // check if all directories exists
+        createDirectories();
+
+        // convert list of document features to list of vectors
+        List<Vector> vectors = getPoints(documentFeatures);
+        writePointsToFile(vectors, conf, new Path(FEATURE_INPUT_PATH + "/file1"));
+
+        FileSystem fs = FileSystem.get(conf);
+        Path path = new Path(CLUSTER_INPUT_PATH + "/part-00000");
+        SequenceFile.Writer writer = new SequenceFile.Writer(fs, conf, path, Text.class, Kluster.class);
+
+        // create initial clusters
+        for (int i = 0; i < k; i++) {
+            Vector vec = vectors.get(i);
+            Kluster cluster = new Kluster(vec, i, new EuclideanDistanceMeasure());
+            writer.append(new Text(cluster.getIdentifier()), cluster);
+        }
+        writer.close();
+
+        // create output path
+        HadoopUtil.delete(conf, output);
+
+        // run k means
+        KMeansDriver.run(
+                conf,
+                input,
+                clusterIn,
+                output,
+                convergenceDelta,
+                maxIterations,
+                runClustering,
+                clusterClassificationThreshold,
+                runSequential
+        );
+
+        // read document cluster assignment
+        readClusters(fs);
+    }
+
+    private void readClusters(FileSystem fs) throws IOException {
+        SequenceFile.Reader reader = new SequenceFile.Reader(fs,
+                new Path(OUTPUT_PATH + Kluster.CLUSTERED_POINTS_DIR
+                        + "/part-m-00000"), conf);
+
+        IntWritable key = new IntWritable();
+        WeightedVectorWritable value = new WeightedVectorWritable();
+        while (reader.next(key, value)) {
+            System.out.println(value.toString() + " belongs to cluster "
+                    + key.toString());
+        }
+
+        reader.close();
+    }
+
+    private List<Vector> getPoints(List<List<Float>> documents) {
+        List<Vector> points = new ArrayList<Vector>();
+
+        for (List<Float> doc: documents) {
+            Vector vec = new RandomAccessSparseVector(doc.size());
+
+            double[] arr = new double[doc.size()];
+            for (int i = 0; i < doc.size(); i++)
+                arr[i] = doc.get(i);
+
+            vec.assign(arr);
+            points.add(vec);
+        }
+
+        return points;
+    }
+
+    private void createDirectories() {
+        File testData = new File(INPUT_PATH);
+        if (!testData.exists()) {
+            testData.mkdir();
+        }
+
+        testData = new File(FEATURE_INPUT_PATH);
+        if (!testData.exists()) {
+            testData.mkdir();
+        }
+    }
+
+
+    public static void writePointsToFile(List<Vector> points, Configuration conf, Path path) throws IOException {
+        FileSystem fs = FileSystem.get(path.toUri(), conf);
+        SequenceFile.Writer writer = new SequenceFile.Writer(fs, conf, path,
+                LongWritable.class, VectorWritable.class);
+        long recNum = 0;
+        VectorWritable vec = new VectorWritable();
+        for (Vector point : points) {
+            vec.set(point);
+            writer.append(new LongWritable(recNum++), vec);
+        }
+        writer.close();
+    }
+
+}
