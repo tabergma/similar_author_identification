df786532d61030d18908af0574f61ec10a9d3007
Tim <tim.draeger@student.hpi.uni-potsdam.de>
2014-11-26 13:53:42 +0100

    added more features and added hadoop and commons-io to build.gradle

--- java/src/main/java/de/hpi/smm/features/FeatureExtractor.java
+++ java/src/main/java/de/hpi/smm/features/FeatureExtractor.java
@@ -3,12 +3,16 @@ package de.hpi.smm.features;
 import java.util.ArrayList;
 import java.util.List;
 
+import de.hpi.smm.features.CapitalLetterFrequencyFeature;
+
 public class FeatureExtractor {
 
     public List<Float> getFeatures(String text) {
         // Initialize features
         WordLengthFeature wordLengthFeature = new WordLengthFeature();
         LetterFrequencyFeature letterFrequencyFeature = new LetterFrequencyFeature();
+        FunctionWordFeature functionWordFeature = new FunctionWordFeature();
+        CapitalLetterFrequencyFeature capitalLetterFrequencyFeature = new CapitalLetterFrequencyFeature();
 
         // Tokenize text
         String[] tokens = text.split("\\s");
@@ -17,12 +21,16 @@ public class FeatureExtractor {
             wordLengthFeature.addWordLength(token.length());
 
             letterFrequencyFeature.evaluateLetterFrequency(token);
+            functionWordFeature.evaluateFunctionWord(token);
+            capitalLetterFrequencyFeature.evaluateCapitalLetterFrequency(token);
         }
 
         // Add features to feature list
         ArrayList<Float> features = new ArrayList<Float>();
         features.add(wordLengthFeature.getAvgWordLength());
         features.addAll(letterFrequencyFeature.getLetterFrequency());
+        features.addAll(functionWordFeature.getFunctionWordFrequency());
+        features.add(capitalLetterFrequencyFeature.getCapitalLetterFrequency());
 
         return features;
     }
