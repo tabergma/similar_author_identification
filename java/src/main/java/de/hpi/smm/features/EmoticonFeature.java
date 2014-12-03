e48b3148c887ad9c8649b779c4b369a0a6b3f3c4
Tanja Bergmann <tabergma@gmail.com>
2014-12-03 16:08:17 +0100

    Change emoticon feature to a binary feature.

--- java/src/main/java/de/hpi/smm/features/EmoticonFeature.java
+++ java/src/main/java/de/hpi/smm/features/EmoticonFeature.java
@@ -1,48 +1,27 @@
 package de.hpi.smm.features;
 
 
-import de.hpi.smm.helper.MutableInt;
-
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class EmoticonFeature extends AbstractFeature {
 
-    private Float[] features;
-    private int emoticonsCount = 0;
-    private Map<String, MutableInt> frequencies;
-    private static List<String> emoticons;
+    private Float[] features = new Float[] {0f};
+    private static List<String> emoticons = getEmoticons();
 
     public EmoticonFeature(float weight) {
         super(weight);
-        
-        this.frequencies = new HashMap<String, MutableInt>(getNumberOfFeatures());
-        for (String emoticon : getEmoticons()){
-            frequencies.put(emoticon, new MutableInt());
-        }
-        this.features = new Float[getNumberOfFeatures()];
     }
 
     @Override
     public void feedToken(String token, String tag) {
-        MutableInt count = frequencies.get(token);
-        if (count != null){
-            count.increment();
-            emoticonsCount++;
+        if (emoticons.contains(token)) {
+            features[0] = 1f;
         }
     }
 
     @Override
     public Float[] getFeatures() {
-        for(int i = 0; i < features.length; i++) {
-            int count = frequencies.get(getEmoticons().get(i)).get();
-            if (count != 0)
-                features[i] = (float) count / this.emoticonsCount;
-            else
-                features[i] = 0f;
-        }
         return features;
     }
 
