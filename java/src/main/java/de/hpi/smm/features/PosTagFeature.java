c6e43784dbc05115c9fee4daa5052f40d0c16abe
Tanja Bergmann <tabergma@gmail.com>
2014-12-03 15:04:05 +0100

    nomalize pos feature.

--- java/src/main/java/de/hpi/smm/features/PosTagFeature.java
+++ java/src/main/java/de/hpi/smm/features/PosTagFeature.java
@@ -8,16 +8,19 @@ import java.util.Map;
 public class PosTagFeature extends AbstractFeature {
 
     Map<String, Integer> tagCount = new HashMap<String, Integer>();
+    int wordCount = 0;
 
     public PosTagFeature(List<String> t) {
         for (String tag : t) {
             tagCount.put(tag, 0);
         }
+        System.out.println(t);
     }
 
     @Override
     public void feedToken(String token, String tag) {
         tagCount.put(tag, tagCount.get(tag) + 1);
+        wordCount++;
     }
 
     @Override
@@ -25,7 +28,11 @@ public class PosTagFeature extends AbstractFeature {
         Float[] features = new Float[tagCount.size()];
         int i = 0;
         for (Integer count : tagCount.values()) {
-            features[i] = (float) count;
+            if (count != 0) {
+                features[i] = (float) count / wordCount;
+            } else {
+                features[i] = 0f;
+            }
             i++;
         }
         return features;
