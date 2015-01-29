cd8c9b676ae280a48f832b906d2b7c84738e7d05
Tanja Bergmann <tabergma@gmail.com>
2015-01-29 17:46:30 +0100

    Some clean up in feature component.

--- clustering/src/main/java/de/hpi/smm/components/FeatureComponent.java
+++ clustering/src/main/java/de/hpi/smm/components/FeatureComponent.java
@@ -16,18 +16,21 @@ import java.util.List;
 public class FeatureComponent {
 
     public static final String SELECT_STATEMENT = "SELECT TABLE1.ID, TABLE1.POSTCONTENT FROM (SELECT %s AS ID, %s AS POSTCONTENT FROM %s LIMIT %d) AS TABLE1 FULL OUTER JOIN SMA1415.SAI_FEATURES AS TABLE2 ON TABLE1.ID = TABLE2.DOCUMENT_ID WHERE TABLE2.FEATURE_0 IS NULL AND TABLE1.POSTCONTENT IS NOT NULL";
+    public static final String ID = "ID";
+    public static final String CONTENT = "POSTCONTENT";
 
     // TODO check file access!!!
     public static void main(String[] args) throws Exception {
-        if (args.length != 1) {
+        if (args.length != 2) {
             System.out.println("Wrong number of arguments!");
             System.out.println("-----------------------------------------------");
             System.out.println("To start the program execute");
-            System.out.println("  java -jar feature_component.jar <data-set-id>");
+            System.out.println("  java -jar feature_component.jar <data-set-id> <limit>");
             System.out.println("-----------------------------------------------");
             System.out.println("data-set-id:");
             System.out.println("  1 -> smm data");
             System.out.println("  2 -> springer data");
+            System.out.println("limit: number of documents for which the features should be extracted");
             return;
         }
 
@@ -48,9 +51,38 @@ public class FeatureComponent {
         DetectorFactory.loadProfile(Config.PROFILES_DIR);
         Detector detector = DetectorFactory.create();
 
-        // Load table
-//        AbstractTableDefinition table = getTable(databaseAdapter, dataSetId);
-//        System.out.println(table.formatCompleteReadStatement());
+        // Get documents
+        System.out.print("Executing SQL statement...");
+        ResultSet resultSet = getResultSet(dataSetId, databaseAdapter);
+        System.out.println("Done.");
+
+        System.out.print("Extracting features...");
+        while(resultSet.next()) {
+            String id = resultSet.getString(ID);
+            String content = resultSet.getString(CONTENT);
+
+            if (content != null) {
+                // clean text
+                content = html2text(content);
+
+                // detect language
+                detector.append(content);
+                String lang = detector.detect();
+
+                // extract features
+                List<Float> features = featureExtractor.getFeatures(content, lang);
+
+                // write features
+                write(databaseAdapter, features, id, dataSetId);
+            }
+        }
+        System.out.println("Done.");
+
+        // Close database connection
+        databaseAdapter.closeConnection();
+    }
+
+    private static ResultSet getResultSet(int dataSetId, DatabaseAdapter databaseAdapter) {
         String idColumn = null, contentColumn = null, tableName = null;
         if (dataSetId == 1){
             idColumn = SchemaConfig.SMM_ID;
@@ -63,64 +95,11 @@ public class FeatureComponent {
         }
         String statement = String.format(SELECT_STATEMENT, idColumn, contentColumn, tableName, 1000);
 
-        ResultSet resultSet = databaseAdapter.executeQuery(statement);
-
-        int i = 0;
-        // read content and extract features
-        while(resultSet.next()) {
-            String id = resultSet.getString(SchemaConfig.SMM_ID);
-            String content = null;
-            content = resultSet.getString(SchemaConfig.SMM_CONTENT);
-//            if (dataSetId == 1) {
-//                content = resultSet.getString(SchemaConfig.SMM_CONTENT);
-//            } else if (dataSetId == 2){
-//                content = resultSet.getString(SchemaConfig.SPINN3R_CONTENT);
-//            }
-
-            if (content != null) {
-                content = html2text(content);
-                i++;
-                // detect language
-                detector.append(content);
-                String lang = detector.detect();
-
-                // extract features
-                List<Float> features = featureExtractor.getFeatures(content, lang);
-
-                // write features
-                write(databaseAdapter, features, id, dataSetId);
-            }
-            if (i == 10){
-                break;
-            }
-        }
-
-        // Close database connection
-        databaseAdapter.closeConnection();
+        return databaseAdapter.executeQuery(statement);
     }
 
-    public static String html2text(String html) {
+    private static String html2text(String html) {
         return Jsoup.parse(html).text();
-    }
-
-    private static AbstractTableDefinition getTable(DatabaseAdapter databaseAdapter, int dataSetId) {
-        Schema schema = SchemaConfig.getWholeSchema(dataSetId);
-        databaseAdapter.setSchema(schema);
-
-        AbstractTableDefinition featureTableDefinition = schema.getTableDefinition(SchemaConfig.getFeatureTableName());
-
-        JoinedTableDefinition joinedTableDefinition = null;
-        if (dataSetId == 1) { // smm
-            AbstractTableDefinition documentTableDefinition = schema.getTableDefinition(SchemaConfig.getSmmTableName());
-            joinedTableDefinition = new JoinedTableDefinition(documentTableDefinition, SchemaConfig.SMM_ID, featureTableDefinition, SchemaConfig.DOCUMENT_ID);
-            joinedTableDefinition.addWhereClause(String.format("%s = null", SchemaConfig.DOCUMENT_ID));
-        } else if (dataSetId == 2) { // spinn3r
-            AbstractTableDefinition documentTableDefinition = schema.getTableDefinition(SchemaConfig.getSpinn3rTableName());
-            joinedTableDefinition = new JoinedTableDefinition(documentTableDefinition, SchemaConfig.SPINN3R_ID, featureTableDefinition, SchemaConfig.DOCUMENT_ID);
-            joinedTableDefinition.addWhereClause(String.format("%s = null", SchemaConfig.DOCUMENT_ID));
-        }
-        joinedTableDefinition.setJoinType(" FULL OUTER ");
-        return databaseAdapter.getReadTable(joinedTableDefinition);
     }
 
     private static void write(DatabaseAdapter databaseAdapter, List<Float> features, String documentId, int dataSetId) {
