262ef3056a606ae5aa0a0b44f5f6d937fe8b6857
Joseph B <joseph.bethge@student.hpi.uni-potsdam.de>
2015-02-01 12:26:42 +0100

    jar created

---
+++ src/main/java/de/hpi/smm/components/HelpComponent.java
@@ -0,0 +1,14 @@
+package de.hpi.smm.components;
+
+public class HelpComponent {
+    public static void main(String[] args) {
+        System.out.println("This is the help for all components for similar author identification.");
+        System.out.println("Use the following lines to call each component, usually you want to start with the first one.");
+        System.out.println("  java -cp similar_author_identification.jar de.hpi.smm.components.FeatureComponent");
+        System.out.println("  java -cp similar_author_identification.jar de.hpi.smm.components.MahoutComponent");
+        System.out.println("  java -cp similar_author_identification.jar de.hpi.smm.components.ResultComponent");
+        System.out.println("  java -cp similar_author_identification.jar de.hpi.smm.components.LabelComponent");
+        System.out.println("  java -cp similar_author_identification.jar de.hpi.smm.components.ClusterComponent");
+        System.out.println("  java -cp similar_author_identification.jar de.hpi.smm.components.SvmComponent");
+    }
+}
